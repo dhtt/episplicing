996d193ce5be764abb2c9f68b8cf57c964ea5a48
Trang Do <dhthutrang@gmail.com>
2020-04-03 11:57:09 +0200

    Add and change files

---
+++ bisulfite_seq/RRBS/processing_FMRC.sh
@@ -0,0 +1,25 @@
+#Processing FM and RC from Roadmaps Epigenomes#
+##Prepare chromosomes and postions##
+awk '{split (FILENAME,name,"."); split ($0,a); print name[1]"\t"a[1]"\t+"}' *.fm > all_chroms.txt
+
+##Separate column in FM and RC into files for selected epigenomes##
+while read line
+do
+    (idx=$(echo $line | awk '{print $1}')
+    NAME=$(echo $line | awk '{print $2;}')
+    echo $idx
+    echo $NAME
+    awk -v i="$idx" '{print $i}' FM/*.fm > FM/"$NAME.txt"
+    awk -v i="$idx" '{print $i}' RC/*.rc > RC/"$NAME.txt") &
+done < epi_ids.txt
+wait
+echo "===> Finished separating files"
+
+##Generating counts from FM and RC for each epigenome##
+for f in FM/E*.txt
+do
+    NAME="${f#*/}"
+    paste -d"\t\t" all_chroms.txt FM/$NAME RC/$NAME > "summary/$NAME"
+done
+wait
+echo "===> Finished generating counts for epigenomes"
