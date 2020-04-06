a6d329f49169c07f6cb696cc1cac6433adb862f7
TrangDo <51161453+dhtt@users.noreply.github.com>
2020-04-06 10:53:49 +0200

    Update count_exon.sh

--- mRNA_seq/count_exon.sh
+++ mRNA_seq/count_exon.sh
@@ -1,11 +1,11 @@
 echo "=====> Begin counting"
 cd "$(dirname "$0")"
 
-if (test -f mRNA_seq_BEDaccessionnumber.txt) && (test -f reference.flattened.ens.gtf)
+if (test -f mRNA_seq_BEDaccessionnumber.txt)
 then
     echo "...File requirement met."
 else
-    echo "Err: Check mRNA_seq_BEDaccessionnumber.txt or reference.flattened.ens.gtf"
+    echo "Err: Check mRNA_seq_BEDaccessionnumber.txt"
     exit 1
 fi
 
@@ -14,21 +14,25 @@ dexseq_count() {
     f="$p".sam
     ACC_NO=${f%%.*}
     echo "counting $ACC_NO" >> log_dexseqcount.txt
-    (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py reference.flattened.ens.gtf $f "$ACC_NO"_count.txt) || (echo "Err: Counting $ACC_NO failed" >> log_dexseqcount.txt)
+    (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py $REFGEN/reference_genome.gtf $f backupcount_NCBI/"$ACC_NO"_count.txt) || (echo "Err: Counting $ACC_NO failed" >> log_dexseqcount.txt)
 }
 
 while read p; 
 do
     dexseq_count &
 done < mRNA_seq_BEDaccessionnumber.txt #temp.txt is accession_number (with order sometimes)
+wait
 
+echo "=====> Changing names"
 # Change name to contain epigenome ID =====================================
-for f in *count.txt
+for f in backupcount_NCBI/*count.txt
 do
-    ACC_NO=${f%%_*}
-    echo "Old name: $ACC_NO"
+    ACC_NO=${f#*/}
+    ACC_NO=${ACC_NO%_*}
     ID=$(grep -ih $ACC_NO temp.txt)
     prefix_ID=${ID%%_*}
-    NEWNAME="$prefix_ID"_"$f"
+    NEWNAME="backupcount_NCBI"/"${prefix_ID}"_"$ACC_NO"_"count.txt"
     mv $f $NEWNAME
 done
+
+echo "=====> Finished counting"
