e075fece1ff0fbce315c5e4dc5afb440a9c50698
Trang Do <dhthutrang@gmail.com>
2020-05-10 12:37:10 +0200

    Chunking execution

--- mRNA_seq/execute_dexseq.sh
+++ mRNA_seq/execute_dexseq.sh
@@ -1,22 +1,32 @@
 cat /dev/null > execute_dexseq.log
 echo "===> START DEXSEQ-ING ALL PAIRS"
+f="$1"
 
 execute_dexseq_parallel() {
     epi1=$(echo $p | awk '{split($0, a, " "); print a[1]}')
     epi2=$(echo $p | awk '{split($0, a, " "); print a[2]}')
-    if (ls . | grep "$epi1" ) && (ls . | grep "$epi2" )
+    if (ls $f | grep "$epi1" ) && (ls $f | grep "$epi2" )
     then (
-        Rscript "$EXP"DEXSeq_analysis.R -a "$epi1" -b "$epi2" -f  "$EXP"backupcount_NCBI/ -g $REFGEN/reference_genome.gtf -n 5
-        echo "DEXseq-ing pair "$epi1"_"$epi2"" >> execute_dexseq.log
+        if (ls "$f"/RData | grep "$epi1"_"$epi2" )
+        then (
+            echo "Pair "$epi1"_"$epi2" already exists" >> execute_dexseq.log
+        )
+        else (
+            Rscript "$EXP"DEXSeq_analysis.R -a "$epi1" -b "$epi2" -f  "$f" -g $REFGEN/reference_genome.gtf -n 8
+            echo "DEXSeq-ing pair "$epi1"_"$epi2"" >> execute_dexseq.log
+        )
+        fi
         )
     else echo "Pair "$epi1"_"$epi2" does not exist" >> execute_dexseq.log
     fi
 }
 
-while read p; 
+for chunk in pair_chunks/*
 do
+    while read p; 
+    do
     execute_dexseq_parallel &
-done < "$EXP"all_pairs.txt
-wait
-echo "===> FINISHED DEXSEQ-ING ALL PAIRS"
-
+    done < $chunk
+    wait
+    echo "===> FINISHED DEXSEQ-ING ALL PAIRS IN "$chunk"" >> execute_dexseq.log
+done
