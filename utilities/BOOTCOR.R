5f94f40b90605f7aa2053ec4a5197b973f4d6c8c
Trang Do <dhthutrang@gmail.com>
2020-06-09 20:51:54 +0200

    Update correlation

--- utilities/BOOTCOR.R
+++ utilities/BOOTCOR.R
@@ -14,8 +14,10 @@ print("===== PREPARE EXP FILE (1 FOR ALL HIS TYPES) =====")
 #   pair.exp_list = vector("list", length(all_pairs.exp))
 #   for (i in 1:length(all_pairs.exp)){
 #     pair.exp = all_pairs.exp[[i]]
-#     pair.exp_list[[i]] = fread(pair.exp)$stat
+#     pair.exp_list[[i]] = fread(pair.exp)$padj
 #   }
+#   pair.exp_list = lapply(pair.exp_list, 
+#                          function(x) ifelse((x > 0 & x <= 0.05 & !is.na(x)), 1.0, 0.0))
 #   pair.exp_list = as.data.table(pair.exp_list)
 #   exp_id = fread("exp_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 #   pair.exp_list = cbind(exp_id, pair.exp_list)
@@ -26,13 +28,12 @@ print("===== PREPARE EXP FILE (1 FOR ALL HIS TYPES) =====")
 
 # all_pairs.exp = get_all_pairs.exp(all_pairs.exp)
 # saveRDS(all_pairs.exp, "all_pairs.exp.RDS")
-all_pairs.exp = readRDS("server_con/all_pairs.exp.RDS")
+all_pairs.exp = readRDS("all_pairs.exp.RDS")
 head(all_pairs.exp)
 all_genes = fread("gene_id.txt", header = FALSE)
 
 #===== PREPARE HIS FILE (6 TOTAL) =====
 print("===== PREPARE HIS FILE (6 TOTAL) =====")
-his_id = fread("flank_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 # get_all_pairs.his <- function(all_pairs.his){
 #   pair.his_list = vector("list", length(all_pairs.his))
 #   for (i in 1:length(all_pairs.his)){
@@ -40,16 +41,18 @@ his_id = fread("flank_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 #     pair.his = all_pairs.his[[i]]
 #     pair.his = fread(pair.his)
 #     pair.his = pair.his %>%
-#       mutate(m_val = if_else(!is.na(V10), 
-#                              true = abs(as.numeric(as.character(V10))), false = 0)) %>%
-#       dplyr::select(m_val)
+#       mutate(m_val = as.numeric(as.character(V10)),
+#              p_val = as.numeric(as.character(V11)),
+#              sig = if_else( abs(m_val) >= 1 & p_val <= 0.05 & !is.na(p_val), 1.0, 0.0)) %>%
+#       select(sig)
 #     pair.his_list[[i]] = pair.his
 #   }
 #   pair.his_list = as.data.table(pair.his_list)
 #   pair.his_list = pair.his_list %>%
 #     group_by(group = gl(n()/2, 2)) %>%
-#     summarise_all(max) %>%
-#     dplyr::select(-group)
+#     summarise_all(min) %>%
+#     select(-group)
+#   his_id = fread("flank_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 #   pair.his_list = cbind(his_id, pair.his_list)
 #   pair.his_list = pair.his_list[order(pair.his_list$V1)]
 #   return(pair.his_list)
@@ -57,7 +60,6 @@ his_id = fread("flank_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 # get_all_pairs.his_list <- function(histone_type_list){
 #   all_pairs.his_list = vector("list", length(histone_type_list))
 #   for (j in 1:length(histone_type_list)){
-#   # for (j in 1:1){
 #     his = histone_type_list[[j]]
 #     print(his)
 #     all_pairs.his = list.files(his, full.names = TRUE)
@@ -71,12 +73,21 @@ his_id = fread("flank_id.txt", sep = '\t', quote=FALSE, header = FALSE)
 n_pairs = 15
 histone_type_list = list("H3K4me1", "H3K4me3", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3")
 # all_pairs.his_list = get_all_pairs.his_list(histone_type_list)
-# saveRDS(all_pairs.his_list, "all_pairs.his_list.RDS")
-all_pairs.his_list = readRDS("server_con/all_pairs.his_list.RDS")
+# save(all_pairs.his_list, "all_pairs.his_list.RDS")
+all_pairs.his_list = readRDS("all_pairs.his_list.RDS")
 
 #===== CORRELATION WITH RANDOMIZATION =====
 # ------------ Execute analysis ------------
 randcor <- function(exp, his){
+  if (length(unique(exp)) > 1 & length(unique(his)) > 1){
+    randcor = RRcor(x = exp, y = his, models=c("Warner", "Warner"), p.list=list(0.2, 0.2))$r[[2]]
+    return(randcor)
+  }
+  else {
+    return(NA)
+  }
+}
+randcor_p <- function(exp, his){
   if (length(unique(exp)) > 1 & length(unique(his)) > 1){
     randcor = RRcor(x = exp, y = his, models=c("Warner", "Warner"), p.list=list(0.2, 0.2))$r[[2]]
     return(randcor)
@@ -93,9 +104,9 @@ pearcor <- function(exp, his){
     return(NA)
   }
 }
-spearcor <- function(exp, his){
+fisher <- function(exp, his){
   if (length(unique(exp)) > 1 & length(unique(his)) > 1){
-    return(cor(exp, his, method = "spearman"))
+    return(fisher.test(exp, his)$p)
   }
   else {
     return(NA)
@@ -111,6 +122,7 @@ chisq <- function(exp, his){
     return(NA)
   }
 }
+
 theta_cor_fisher <- function(data,i){
   xdata <- data[i,]
   return(cor(as.numeric(xdata[, 1][[1]]), as.numeric(xdata[, 2][[1]])))
@@ -132,28 +144,6 @@ bootcor_fisher <- function(dtable, nperm, theta){
   }
   else {
     # print("return 3")
-    return(3.0)
-  }
-}
-
-p_value_calculator <- function(r, nrow){
-  P <- r*sqrt(nrow-2)/sqrt(1-r*r)
-  P <- 2*pt(-abs(P), nrow-2)
-  return(P)
-}
-pearcor_p <- function(exp, his){
-  n = length(exp)
-  if (length(unique(exp)) > 1 & length(unique(his)) > 1 & n >= 3){
-    p_val = p_value_calculator(cor(exp, his, method = "pearson"), n)
-    if (!is.na(p_val) & !is.nan(p_val)){
-      return(p_val) 
-    }
-    else {
-      return(2.0)
-    }
-    
-  }
-  else {
     return(3.0)
   }
 }
@@ -189,22 +179,16 @@ analyze_array <- function(all_pairs.exp, all_pairs.his, method, nperm = 1000, th
         summarise(res = pearcor(exp, his)) %>%
         dplyr::select(res)
     }
-    else if (method == "spearcor"){
+    else if (method == "fisher"){
       res_table = data_table %>%
         group_by(all_pairs.exp$gene_id) %>%
-        summarise(res = spearcor(exp, his)) %>%
+        summarise(res = fisher(exp, his)) %>%
         dplyr::select(res)
     }
     else if (method == "chisq"){
       res_table = data_table %>%
         group_by(all_pairs.exp$gene_id) %>%
         summarise(res = chisq(exp, his)) %>%
-        dplyr::select(res)
-    }
-    else if (method == "pearcor_p"){
-      res_table = data_table %>%
-        group_by(all_pairs.exp$gene_id) %>%
-        summarise(res = pearcor_p(exp, his)) %>%
         dplyr::select(res)
     }
     # all_res_pair[[i]] = res_table
@@ -225,34 +209,18 @@ analyze_array_list <- function(all_pairs.exp, all_pairs.his_list, method){
 }
 
 print("Pearsons correlation")
-# all_res_list.pearcor = analyze_array_list(all_pairs.exp, all_pairs.his_list, "pearcor")
-# saveRDS(all_res_list.pearcor, "all_res_list.pearcor.RDS")
-
-print("Pearsons-p correlation")
-# all_res_list.pearcor_p = analyze_array_list(all_pairs.exp, all_pairs.his_list, "pearcor_p")
-# saveRDS(all_res_list.pearcor_p, "all_res_list.pearcor_p.RDS")
-
-# print("Spearman correlation")
-# all_res_list.spearcor = analyze_array_list(all_pairs.exp, all_pairs.his_list, "spearcor")
-# saveRDS(all_res_list.spearcor, "all_res_list.spearcor.RDS")
-
-# print("Bootstrapped correlation")
-# all_res_list.bootcor_fisher = analyze_array_list(all_pairs.exp, all_pairs.his_list, "bootcor_fisher")
-# saveRDS(all_res_list.bootcor_fisher, "all_res_list.bootcor_fisher.RDS")
-# 
-# print("Chi Square")
-# all_res_list.chisq = analyze_array_list(all_pairs.exp, all_pairs.his_list, "chisq")
-# saveRDS(all_res_list.chisq, "all_res_list.chisq.RDS")
-# 
-# print("Randomized correlation")
-# all_res_list.randcor = analyze_array_list(all_pairs.exp, all_pairs.his_list, "randcor")
-# saveRDS(all_res_list.randcor, "all_res_list.randcor.RDS")
-# print(gsub(":", "-", Sys.time()))
-# 
-
-
-
-
-
-
-
+all_res_list.pearcor = analyze_array_list(all_pairs.exp, all_pairs.his_list, "pearcor")
+saveRDS(all_res_list.pearcor, "all_res_list.pearcor.RDS")
+print("Randomized correlation")
+all_res_list.randcor = analyze_array_list(all_pairs.exp, all_pairs.his_list, "randcor")
+saveRDS(all_res_list.randcor, "all_res_list.randcor.RDS")
+print("Fisher Exact Test")
+all_res_list.fisher = analyze_array_list(all_pairs.exp, all_pairs.his_list, "fisher")
+saveRDS(all_res_list.fisher, "all_res_list.fisher.RDS")
+print("Chi Square")
+all_res_list.chisq = analyze_array_list(all_pairs.exp, all_pairs.his_list, "chisq")
+saveRDS(all_res_list.chisq, "all_res_list.chisq.RDS")
+print("Bootstrapped correlation")
+all_res_list.bootcor_fisher = analyze_array_list(all_pairs.exp, all_pairs.his_list, "bootcor_fisher")
+saveRDS(all_res_list.bootcor_fisher, "all_res_list.bootcor_fisher.RDS")
+print(gsub(":", "-", Sys.time()))
